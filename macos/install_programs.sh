5561e5cf48cd3bafa3d2499e5b17d2c877832298
kskyj <fragilememor@gmail.com>
2022-08-13 17:33:32 +0900

    macos 설정 추가

---
+++ macos/install_programs.sh
@@ -0,0 +1,16 @@
+!#/bin/bash
+# chrome
+brew install --cask google-chrome
+
+# vscode
+# vscode font 'JetBrains Mono','KB금융 본문체'
+brew install --cask visual-studio-code
+
+# intellij community
+brew install --cask intellij-idea-ce
+
+# github client
+brew install --cask github
+
+# postman
+brew install --cask postman
