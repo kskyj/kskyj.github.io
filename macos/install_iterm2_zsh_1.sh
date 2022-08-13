c7ca35c7d780c5c7f8543a1ccd9b494e3a6b1b54
kskyj <fragilememor@gmail.com>
2022-08-13 18:08:19 +0900

    install 분리

---
+++ macos/install_iterm2_zsh_1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# oh-my-zsh, zsh plugin, homebrew, iterm2 설치 및 설정
+
+# command line tool
+echo "# Install apple command line tool"
+xcode-select --install
+
+# homebrew /Users/$USER/.zprofile
+echo "# Install homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# oh-my-zsh
+echo "# Install oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
